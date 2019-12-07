53827760a2f4a2a0c44616d103174b54203782ef
vy3191 <venkyafhs@gmail.com>
2019-12-07 18:01:43 -0500

    Added routes to the application

---
+++ routers/hub.js
@@ -0,0 +1,96 @@
+const express = require("express");
+const hubs = require("../hubs/hubs-model");
+
+const router = express.Router();
+
+
+
+router.get("/", (req, res) => {
+  const opts = {
+    limit:req.query.limit
+  }
+  hubs.find()
+    .then(hubs => {
+      res.status(200).json(hubs)
+    })
+    .catch(error => {
+      console.log(error)
+      res.status(500).json({
+        message: "Error retrieving the hubs",
+      })
+    })
+})
+
+router.get("/:id", (req, res) => {
+  hubs.findById(req.params.id)
+    .then(hub => {
+      if (hub) {
+        res.status(200).json(hub)
+      } else {
+        res.status(404).json({ message: "Hub not found" })
+      }
+    })
+    .catch(error => {
+      console.log(error)
+      res.status(500).json({
+        message: "Error retrieving the hub",
+      })
+    })
+})
+
+router.post("/", (req, res) => {
+  if (!req.body.name) {
+    return res.status(400).json({ message: "Missing hub name" })
+  }
+
+  hubs.add(req.body)
+    .then(hub => {
+      res.status(201).json(hub)
+    })
+    .catch(error => {
+      console.log(error)
+      res.status(500).json({
+        message: "Error adding the hub",
+      })
+    })
+})
+
+router.put("/:id", (req, res) => {
+  if (!req.body.name) {
+    return res.status(400).json({ message: "Missing hub name" })
+  }
+
+  hubs.update(req.params.id, req.body)
+    .then(hub => {
+      if (hub) {
+        res.status(200).json(hub)
+      } else {
+        res.status(404).json({ message: "The hub could not be found" })
+      }
+    })
+    .catch(error => {
+      console.log(error)
+      res.status(500).json({
+        message: "Error updating the hub",
+      })
+    })
+})
+
+router.delete("/:id", (req, res) => {
+  hubs.remove(req.params.id)
+    .then(count => {
+      if (count > 0) {
+        res.status(200).json({ message: "The hub has been nuked" })
+      } else {
+        res.status(404).json({ message: "The hub could not be found" })
+      }
+    })
+    .catch(error => {
+      console.log(error)
+      res.status(500).json({
+        message: "Error removing the hub",
+      })
+    })
+})
+
+module.exports = router;

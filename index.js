823ccdcea319cbc9ae71857c1d44f4ebc826122c
vy3191 <venkyafhs@gmail.com>
2019-12-12 21:58:10 -0500

    Added dotenv and modified index.js

--- index.js
+++ index.js
@@ -2,10 +2,26 @@ const express = require("express");
 const hubRouter = require("./routers/hub");
 const welcomRouter = require("./routers/welcome");
 
+
+
+const dotenv = require("dotenv");
+dotenv.config();
+
+const HOST = process.env.HOST || "0.0.0.0";
+const PORT = process.env.PORT || 4000;
+
+
 const server = express();
 
 server.use(express.json());
-server.use("/", welcomRouter);
+server.get("/", (req,res) => {
+    res.status(200).json({
+       Msg:"Successful",
+       CONTENT:process.env.CONTENT,
+       SECRET_KEY: process.env.SECRET_KEY
+    })
+})
+server.use("/api/", welcomRouter);
 server.use("/api/hubs", hubRouter);
 
 
@@ -13,6 +29,6 @@ server.use("/api/hubs", hubRouter);
 // add an endpoint that returns all the messages for a hub
 // add an endpoint for adding new message to a hub
 
-server.listen(4000, () => {
+server.listen(PORT, HOST, () => {
   console.log("\n*** Server Running on http://localhost:4000 ***\n")
 })
